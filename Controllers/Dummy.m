05ac2bfaf2014775543bb89b11621f9c42fd14e2
Alex Tidy <banagon21@icloud.com>
2026-01-28 11:58:26 +0000

    Delete Controllers directory

--- Controllers/Dummy.m
+++
@@ -1,9 +0,0 @@
-function [throttle, gimbal_pitch, gimbal_yaw, nose_AoA] = Dummy(state, control)
-%%%%% Dummy control, does nothing. For testing.
-
-throttle = 0.5;
-gimbal_pitch = 0;
-gimbal_yaw = 0;
-nose_AoA = 0;
-
-end
