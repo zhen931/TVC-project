8c900d50fa023f42756e42424032278fac5c41cd
Alex Tidy <banagon21@icloud.com>
2026-02-01 15:50:09 +0000

    Quaternion conversion functions

---
+++ Thrust Vectoring Sim/Simulation/Engines/BodytoWorld.m
@@ -0,0 +1,17 @@
+function [x] = BodytoWorld(q, x)
+%%%%% Transforms body quaternion to world axes
+
+q1 = q(1);
+q2 = q(2);
+q3 = q(3);
+q4 = q(4);
+x1 = x(1);
+x2 = x(2); 
+x3 = x(3);
+
+x = [
+    (q1^2+q2^2-q3^2-q4^2)*x1 + 2*(q2*q3-q1*q4)*x2 + 2*(q2*q4+q1*q3)*x3;
+    2*(q2*q3+q1*q4)*x1 + (q1^2-q2^2+q3^2-q4^2)*x2 + 2*(q3*q4-q1*q2)*x3;
+    2*(q2*q4-q1*q3)*x1 + 2*(q3*q4+q1*q2)*x2 + (q1^2-q2^2-q3^2+q4^2)*x3
+];
+end
